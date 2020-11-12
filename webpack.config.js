3b9599ee16b44d24abfecb5ff356a1e51463c733
Mitch McDermott <mmcdermott319@gmail.com>
2020-11-12 14:59:20 -0600

    created webpack config and reorganized files

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    filename: 'bundle.jsx',
+    path: path.resolve(__dirname, './client/dist')
+  },
+  module: {
+    rules: [{ test: /\.jsx?/, exclude: /node_modules/, loader: 'babel-loader' }]
+  },
+  watch: true,
+  node: {
+    fs: 'empty',
+    net: 'empty'
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+}
+
