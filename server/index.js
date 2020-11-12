dfbeec852bfa50439d36c7dc72c81f247047e8f5
Mitch McDermott <mmcdermott319@gmail.com>
2020-11-12 13:05:25 -0600

    cleaned up code

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('../database-mysql');
+const db = require('./db');
 
 const app = express();
 const PORT = 3000;
