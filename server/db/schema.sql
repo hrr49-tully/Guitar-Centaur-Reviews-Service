a3438c02dae481bddef525cc7998cf262e26f0ac
Mitch McDermott <mmcdermott319@gmail.com>
2020-11-12 16:42:31 -0600

    Added mysql, minor changes made to schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS guitarReviews;
 CREATE DATABASE guitarReviews;
 
 USE guitarReviews;
@@ -8,11 +9,11 @@ CREATE TABLE reviews (
   contents text,
   stars int not null,
   user varchar(25),
-  experience varchar(10,
+  experience varchar(10),
   dateSubmitted timestamp,
   location varchar(30),
-  upVotes int(3) default '0' not null,
-  downVotes int(3) default '0' not null,
+  upVotes int(3) not null default 0,
+  downVotes int(3) not null default 0,
   pros varchar(75),
   cons varchar(75),
   wouldRecommend boolean not null
@@ -21,11 +22,11 @@ CREATE TABLE reviews (
 CREATE TABLE pros (
   proID int not null auto_increment primary key,
   description varchar(25) not null,
-  count int default '0' not null
+  count int not null default 0
 );
 
 CREATE TABLE cons (
   conID int not null auto_increment primary key,
   description varchar(25) not null,
-  count int default '0' not null
+  count int not null default 0
 );
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const db = require('./db');
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
