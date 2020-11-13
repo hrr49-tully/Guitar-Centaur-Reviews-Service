883eb453ae502d0ceeda125811cc70c8fb35d2ed
Mitch McDermott <mmcdermott319@gmail.com>
2020-11-12 19:32:20 -0600

    Server handles api requests

--- server/server.js
+++ server/server.js
@@ -10,6 +10,29 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../client/dist'));
 
+app.get('/guitar/reviews', (req, res) => {
+  db.query('select * from reviews', (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log('data from database query:', JSON.stringify(data));
+      res.send(JSON.stringify(data));
+    }
+  })
+})
+
+app.post('/guitar/reviews', (req, res) => {
+  db.query('insert into reviews (title, contents, stars, user, experience, dateSubmitted, location, upVotes, downVotes, pros, cons, wouldRecommend) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [req.body.title, req.body.contents, req.body.stars, req.body.user, req.body.experience, req.body.dateSubmitted, req.body.location, req.body.upVotes, req.body.downVotes, req.body.pros, req.body.cons, req.body.wouldRecommend], (err, data) => {
+    if (err) {
+      console.log('post error:', err);
+      throw err;
+    } else {
+      console.log('post successful');
+      res.send(data);
+    }
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
