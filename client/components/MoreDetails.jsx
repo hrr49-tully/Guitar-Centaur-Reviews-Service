d76d431cde0e77a8b3094472d2d411dadbf5d9d9
Mitch McDermott <mmcdermott319@gmail.com>
2020-11-20 02:18:05 -0600

    More styling and new details component

---
+++ client/components/MoreDetails.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MoreDetails = (props) => {
+  const review = props;
+  return (
+    <section>
+      More Details
+      {/* <span class="pr-caret-icon"><svg viewBox="-9 -10 52 52" focusable="false" role="presentation"><g><polyline class="pr-caret-icon__line" fill="none" stroke="#333" stroke-width="4" points="23.7,31.5 8.3,16 23.7,0.5 "></polyline></g></svg></span> */}
+    </section>
+  )
+}
+
+      // Pros: {review.pros}
+      // <br/>
+      // Cons: {review.cons}
+      // <p>Describe Yourself: {review.experience}</p>
+
+export default MoreDetails;
