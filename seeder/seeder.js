74cf8db726be6fa2ad9deca405ab2e82af866cf5
dwwr <dwwarnerreyes@gmail.com>
2020-11-22 12:41:16 -0800

    made changes to work with derek's proxy

--- seeder/seeder.js
+++ seeder/seeder.js
@@ -120,7 +120,7 @@ for (let i = 0; i < cons.length; i++) {
 // seeds reviews into database
 for (let i = 0; i < reviews.length; i++) {
   let review = reviews[i];
-  db.query('insert into reviews (id, title, contents, stars, user, experience, dateSubmitted, location, upVotes, downVotes, pros, cons, wouldRecommend) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [review.id, review.title, review.contents, review.stars, review.user, review.experience, review.dateSubmitted, review.location, review.upVotes, review.downVotes, review.pros, review.cons, review.wouldRecommend], (err, data) => {
+  db.connection.query('insert into reviews (id, title, contents, stars, user, experience, dateSubmitted, location, upVotes, downVotes, pros, cons, wouldRecommend) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [review.id, review.title, review.contents, review.stars, review.user, review.experience, review.dateSubmitted, review.location, review.upVotes, review.downVotes, review.pros, review.cons, review.wouldRecommend], (err, data) => {
     if (err) {
       console.error(err);
       throw err;
@@ -133,7 +133,7 @@ for (let i = 0; i < reviews.length; i++) {
 // seeds pros into database
 for (let i = 0; i < prosObjects.length; i++) {
   let pro = prosObjects[i];
-  db.query('insert into pros (proID, description, count) values (?, ?, ?)', [pro.proID, pro.description, pro.count], (err, data) => {
+  db.connection.query('insert into pros (proID, description, count) values (?, ?, ?)', [pro.proID, pro.description, pro.count], (err, data) => {
     if (err) {
       console.error(err);
       throw err;
@@ -146,7 +146,7 @@ for (let i = 0; i < prosObjects.length; i++) {
 // seeds cons into database
 for (let i = 0; i < consObjects.length; i++) {
   let con = consObjects[i];
-  db.query('insert into cons (conID, description, count) values (?, ?, ?)', [con.conID, con.description, con.count], (err, data) => {
+  db.connection.query('insert into cons (conID, description, count) values (?, ?, ?)', [con.conID, con.description, con.count], (err, data) => {
     if (err) {
       console.error(err);
       throw err;
@@ -157,4 +157,4 @@ for (let i = 0; i < consObjects.length; i++) {
 }
 
 
-db.end();
+db.connection.end();
