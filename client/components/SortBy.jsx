cae85711e326d50d172af7c44c38e4cdaff2520b
Mitch McDermott <mmcdermott319@gmail.com>
2020-11-27 18:48:01 -0600

    Up/down vote buttons styling complete

--- client/components/SortBy.jsx
+++ client/components/SortBy.jsx
@@ -2,27 +2,8 @@ import React from 'react';
 import styles from './css/SortByStyles.css';
 
 const SortBy = () => {
-  const [sortOption, setSortOption] = React.useState('Most Recent');
-  const switchSort = (input) => {
-    setShowOptions(input);
-  };
-  const options = ['Most Recent', 'Most Helpful', 'Highest Rated', 'Lowest Rated', 'Oldest'];
-
   return (
-    <div>
-      <div>Reviewed by {props.reviews.length} customers</div>
-      <select className={styles.sortByMenu}>
-        <optgroup label="Sort Reviews By:">
-          <option>Most Recent</option>
-          <option>Most Helpful</option>
-          <option>Highest Rated</option>
-          <option>Lowest Rated</option>
-          <option>Oldest</option>
-        </optgroup>
-      </select>
-    </div>
-  );
-};
-
+    <div></div>
+  )
 
 export default SortBy;
