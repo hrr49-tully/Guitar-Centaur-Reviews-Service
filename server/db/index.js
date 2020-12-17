50802c4fd7093d7079da85bbc0cc4e0d5cf455d7
Mitch McDermott <mmcdermott319@gmail.com>
2020-12-17 00:28:54 -0600

    cleaned up code

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 
-const host  = 'localhost';
-const port  = 3004;
-const url   = `http://${host}:${port}`;
+const host = 'localhost';
+const port = 3004;
+const url = `http://${host}:${port}`;
 const user = 'root';
 const database = 'guitarReviews';
 const password = '';
